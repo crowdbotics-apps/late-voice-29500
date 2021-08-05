ef3c7a86ad59ece1c838338a2c05ab9f72831260
Crowdbotics <team@crowdbotics.com>
2021-08-05 09:42:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "late_voice_29500Identifier",
+  url: "https://late_voice_29500Identifier.botics.co",
+  api: "https://late_voice_29500Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
